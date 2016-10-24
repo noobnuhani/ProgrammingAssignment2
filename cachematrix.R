c714b08832abae4c5d44c4cb8c980a4f6ce2b184
noonnuhani <aashish.nuhani@gmail.com>
2016-10-24 10:40:50 +0530

    Commiting the cachematrix file and have changed the function andll, But why is this commit for where would it be visible

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# makeCacheMatrix is a function that returns a list of functions
+   # * setMatrix      set the value 
+   # * getMatrix      get the value 
+   # * cacheInverse   get the cahced value (inverse of matrix)
+   # * getInverse     get the cahced value (inverse of matrix)
+  
 makeCacheMatrix <- function(x = matrix()) {
+  
+  cache <- NULL
+         # store a matrix
+         setMatrix <- function(newValue) {
+               x <<- newValue
+               # since the matrix is assigned a new value,remove the vache
+                 cache <<- NULL
+           }
+        
+         # returns the stored matrix
+           getMatrix <- function() {
+                 x
+           }
+          
+        # cache the given argument 
+           cacheInverse <- function(solve) {
+               cache <<- solve
+           }
+            
+        # get the cached value
+               getInverse <- function() {
+                   cache
+           }
+              
+        list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
+      }
 
-}
 
-
-## Write a short comment describing this function
+## Inverse of a matrix 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  # get the cached value
+     inverse <- x$getInverse()
+       # if a cached value exists return it
+         if(!is.null(inverse)) {
+               message("getting cached data")
+               return(inverse)
+           }
+      # otherwise get the matrix, caclulate the inverse and store it in
+         # the cache
+         data <- x$getMatrix()
+         inverse <- solve(data)
+         x$cacheInverse(inverse)
+         
+         # return the inverse
+         inverse    
+  
+  
+    ## Return a matrix that is the inverse of 'x'
 }
